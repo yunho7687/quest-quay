62249ac0772ef53a3c3e54348137eec16aa59315
Yunho Ding <gurumindc123@gmail.com>
2024-04-19 23:42:58 +0800

    move the templates file to app add index.html file

--- .flaskenv
+++ .flaskenv
@@ -1,4 +1,4 @@
-FLASK_APP=microblog.py
+FLASK_APP=quest_quay.py
 FLASK_ENV=development
 FLASK_DEBUG=1
 FLASK_RUN_PORT=5505
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,12 @@
 from app import app
+from flask import render_template
+
 @app.route('/')
+def roof():
+    return "Hellooooooo, World!<p style='color:red' >Welcome to CITS5505 good luck. </p>"
+
+
 @app.route('/index')
 def index():
-    return "Hellooooooo, World!<p style='color:red' >Welcome to CITS5505 good luck. </p>"
+    return render_template("index.html")
 
