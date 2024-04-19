dcdd068143fdfb45fc7b3aa8e06cc2454acdf839
Yunho Ding <gurumindc123@gmail.com>
2024-04-20 02:32:48 +0800

    optimize the structure of the directory

--- app/routes.py
+++ app/routes.py
@@ -2,10 +2,6 @@ from app import app
 from flask import render_template
 
 @app.route('/')
-def roof():
-    return "Hellooooooo, World!<p style='color:red' >Welcome to CITS5505 good luck. </p>"
-
-
 @app.route('/index')
 def index():
     title = ""
