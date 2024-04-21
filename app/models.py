ffd23af8613ccf30a47d27b856ba9ad646d6ec20
Yunho Ding <gurumindc123@gmail.com>
2024-04-21 17:55:27 +0800

    Playing with the Database

--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
+from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
+
 
 class User(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -11,5 +13,24 @@ class User(db.Model):
                                              unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
 
+    posts: so.WriteOnlyMapped['Post'] = so.relationship(
+        back_populates='author')
+    # back_populates: reference the name of the relationship attribute on the other side
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+
+class Post(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    body: so.Mapped[str] = so.mapped_column(sa.String(140))
+    timestamp: so.Mapped[datetime] = so.mapped_column(
+        index=True, default=lambda: datetime.now(timezone.utc))
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id),
+                                               index=True)
+
+    author: so.Mapped[User] = so.relationship(back_populates='posts')
+    # back_populates: reference the name of the relationship attribute on the other side
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
