3eedbf293d8cd69bbd9c0643712c41dd12db0630
Yunho Ding <gurumindc123@gmail.com>
2024-04-30 20:36:30 +0800

    Update README with instructions for setting up mock data and test account

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     DEBUG = True
     # DEBUG = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+        'sqlite:///' + os.path.join(basedir, 'app.db')  # set the databse name to app.db
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
