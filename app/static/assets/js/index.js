42b08a4277994a087cc50d15a471d30b86d96742
Yunho <gurumindc123@gmail.com>
2024-05-12 04:50:37 +0800

    chore: Update style.css to add border to .form-control

--- app/static/assets/js/index.js
+++ app/static/assets/js/index.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
-  const submitPost = $("#submit-post");
-  const submit = $("#submit");
+  const submitPostBtn = $("#submit-post-btn");
+  const submit = $("#submit-post");
   const navCreatePostBtn = $("#nav-create-post");
 
-  submitPost.click(function (e) {
+  submitPostBtn.click(function (e) {
     e.preventDefault();
     submit.click();
   });
-  navCreatePostBtn.click(function(e){
-    e.preventDefault()
-     $(".modal-backdrop.fade.show").addClass("blur-effect opacity-100");
-  })
+  navCreatePostBtn.click(function (e) {
+    e.preventDefault();
+    $(".modal-backdrop.fade.show").addClass("blur-effect opacity-100");
+  });
 });
